76208b20b20c5bd6f963fbe2eb1506f6a2340b6f
Dotan Nahum <jondotan@gmail.com>
2015-09-20 21:13:42 +0300

    initial import

---
+++ README.md
@@ -0,0 +1,37 @@
+# Sticker
+
+
+Sticker is an [Armor](https://github.com/jondot/armor) based reverse-proxy, that
+will use a `backend` and an `add_headers` and `remove_headeres` configuration,
+in order to stick or remove headers onto a web host.
+
+This is needed when you don't have a control over a service, but need to
+stick a few headers on to it.
+
+
+### Examples
+
+You can put Sticker on a free Heroku instance, or an AWS micro instance, and
+then handle some of these use cases:
+
+* Stick arbitrary, custom headers on S3 resources
+* Fix bad RSS feeds (bad xml/json/atom headers)
+* Fix/force control of bad caching with resources (proper datetime)
+* Set caching policies on external resources you don't control, infront of CDN
+you do control (forcefully CDN another party's resources)
+
+And so on. Mostly, fixing other people's mistakes, and lack of features :)
+
+
+
+# Contributing
+
+Fork, implement, add tests, pull request, get my everlasting thanks and a respectable place here :).
+
+
+# Copyright
+
+Copyright (c) 2015 [Dotan Nahum](http://gplus.to/dotan) [@jondot](http://twitter.com/jondot). See MIT-LICENSE for further details.
+
+
+
--- main.go
+++ main.go
@@ -1,20 +1,64 @@
-
 package main
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jondot/armor"
 	"time"
+	"net/url"
+	"net/http/httputil"
+"net/http"
 )
+
+type HeaderPokingTransport struct {
+	AddHeaders map[interface{}]interface{}
+	RemoveHeaders map[interface{}]interface{}
+
+}
+
+func (t *HeaderPokingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Host = "" // force using url host
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil{
+		return resp, err
+	}
+
+	for k, _ := range(t.RemoveHeaders){
+		resp.Header.Del(k.(string))
+	}
+
+	for k, v := range(t.AddHeaders){
+		resp.Header.Add(k.(string), v.(string))
+	}
+
+	return resp, nil
+}
+
 
 func main() {
 	m := armor.New("sticker", "1.0")
 	r := m.GinRouter()
 
-	r.GET("/", func(c *gin.Context) {
+	backend := m.Config.GetString("backend")
+	addHeaders := m.Config.Get("add_headers").(map[interface{}]interface{})
+	removeHeaders := m.Config.Get("remove_headers").(map[interface{}]interface{})
+
+	u, err := url.Parse(backend)
+	if err != nil {
+		m.Log.Fatalf("Cannot parse backend url %s, error: %s", u, err)
+	}
+
+
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	proxy.Transport = &HeaderPokingTransport{
+		RemoveHeaders:removeHeaders,
+		AddHeaders:addHeaders,
+	}
+
+	r.GET("/api/*any", func(c *gin.Context) {
 		defer m.Metrics.Timed("timed.request", time.Now())
-		m.Metrics.Inc("foobar")
-		c.String(200, "hello world")
+		m.Metrics.Inc("request")
+		c.Request.URL.Path = c.Request.URL.Path[5:]
+		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
 	m.Run(r)
